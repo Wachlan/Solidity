20b9437b635df6ca7e4cd355735192525052f8f7
Wachlan <wachlanchow@gmail.com>
2022-07-30 22:18:16 +1000

    Add functions to send and receive Ether
    Owners can submit, approve, revoke and execute a transaction once the number of required approvals is reached.

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -20,13 +20,34 @@ contract MultiSigWallet {
   address[] public owners;
   mapping(address => bool) public isOwner;
   uint public required;  // Required number of approvals to send a transaction
+  mapping(uint => mapping(address => bool)) public approved;  // For a transaction ID, keep track of which addresses have approved a transaction
 
   transaction[] public transactions;
+
+  event Deposit(address indexed sender, uint value);
+  event Approve(address indexed owner, uint indexed id);
+  event Submit(address indexed owner, uint indexed id);
+  event Revoke(address indexed  owner, uint indexed id);
+  event Execute(uint indexed id);
   
+  // Only allow access for wallet owners 
   modifier onlyOwner() {
     require(isOwner[msg.sender], "Only owner");
     _;
   }
+
+  // Make sure a transaction exists
+  modifier tranExists(uint id) {
+    require(id < transactions.length);
+    _;
+  }
+
+  // Make sure a transaction has not been executed
+  modifier notExecuted(uint id) {
+    require(!transactions[id].executed, "Already executed");
+    _;
+  }
+
 
   constructor (address[] memory _owners, uint requiredApprovals) {
     require(_owners.length > 0, "Cannot have 0 owners");
@@ -44,8 +65,48 @@ contract MultiSigWallet {
 
   }
 
+  receive() external payable {
+    emit Deposit(msg.sender, msg.value);
+  }
 
+  function submit(address _to, uint _value, bytes calldata _data) external onlyOwner {
+    transaction memory trans;
+    trans.to = _to;
+    trans.value = _value;
+    trans.data = _data;
+    trans.executed = false;
+    transactions.push(trans);
+    emit Submit(msg.sender, transactions.length - 1);
+  }
 
+  function approve(uint id) external onlyOwner tranExists(id) notExecuted(id) {
+    approved[id][msg.sender] = true;
+    emit Approve(msg.sender, id);
+  }
+
+  function revokeApproval(uint id) external onlyOwner tranExists(id) notExecuted(id) {
+    approved[id][msg.sender] = false;
+    emit Revoke(msg.sender, id);
+  }
+
+  function getApprovalNumber(uint id) public returns (uint aprNum) {
+    for (uint i = 0; i < owners.length; i++) {
+      if (approved[id][owners[i]] = true) {
+        aprNum += 1;
+      }
+    }
+  }
+
+  function execute(uint id) external onlyOwner tranExists(id) notExecuted(id) {
+    uint approvalNum = getApprovalNumber(id);
+    require(approvalNum >= required, "Not enough approvals");
+
+    transaction storage Trans = transactions[id];
+    Trans.executed = true;
+    (bool success, ) = Trans.to.call{value: Trans.value}(Trans.data);
+    require(success,"Transaction failed");
+    emit Execute(id);
+  }
 
 
 }
